e18b86c64a0077101eeebc046009a8b77e913bed
mateoU1297 <castro.mateo@correounivalle.edu.co>
2026-04-26 08:03:28 -0500

    first commit

---
+++ src/main/java/com/pragma/ms_person/MsPersonApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.ms_person;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPersonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragma/ms_person/MsPersonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragma.ms_person;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPersonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
